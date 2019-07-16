a8c8ee67a07da87b2cb461145ca7bcde4b3203fc
Tagayasu <ig7dpop0kbz6@gmail.com>
2019-07-16 20:19:14 +0900

    [add] animationImagesを返却するようにした #2

--- GIFAnimationView/UIImage+Extension.swift
+++ GIFAnimationView/UIImage+Extension.swift
@@ -9,11 +9,20 @@
 import Foundation
 
 extension UIImage {
-    public static func animationImage(name: String) -> UIImage? {
-        
+    public static func animationImages(name: String) -> [UIImage]? {
+        guard let imageData = imageData(name: name),
+        let cgImageSource = CGImageSourceCreateWithData(imageData as CFData, nil) else { return nil }
+        let cgImageSourceCount = CGImageSourceGetCount(cgImageSource)
+        return (0...cgImageSourceCount).compactMap { (index) -> UIImage? in
+            guard let cgImage = CGImageSourceCreateImageAtIndex(cgImageSource, index, nil) else { return nil }
+            return UIImage(cgImage: cgImage)
+        }
+    }
+}
+
+private extension UIImage {
+    static func imageData(name: String) -> Data? {
         guard let gifPath = Bundle.main.url(forResource: name, withExtension: "gif") else { return nil }
-        guard let data = try? Data(contentsOf: gifPath) else { return nil }
-        
-        return UIImage(data: data)
+        return try? Data(contentsOf: gifPath)
     }
 }
